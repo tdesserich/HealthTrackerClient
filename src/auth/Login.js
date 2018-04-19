1c62390bb979ba5e13df4434384ccc1d05dd0201
Tracy Desserich <tdesserich@gmail.com>
2018-04-18 22:48:22 -0400

    modified signup and login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { 
-    Form, 
-    FormGroup, 
-    Label, 
-    Input, 
-    Button } 
-from 'reactstrap';
+import validator from 'react-validation';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button,
+    FormFeedback
+}
+    from 'reactstrap';
 
 
 class Login extends Component {
@@ -13,28 +16,60 @@ class Login extends Component {
         super(props)
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            usernameValid: true,
+            passwordValid: true
         };
     }
 
     handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        })
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({ [name]: value },
+            () => {
+                this.validateField(name, value)
+            });
+
     }
 
+    validateField(fieldName, value) {
+
+        let usernameValid = this.state.usernameValid;
+        let passwordValid = this.state.passwordValid;
+
+        switch (fieldName) {
+            case 'username':
+                usernameValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                break;
+            case 'password':
+                passwordValid = value.length >= 6;
+                break;
+            default:
+                break;
+        }
+
+        this.setState({
+            usernameValid: usernameValid,
+            passwordValid: passwordValid
+        });
+    }
+
+
+
     handleSubmit = (event) => {
-        fetch("http://localhost:3000/user/signin", {
-            method: 'POST',
-            body: JSON.stringify({user: this.state}),
-            headers: new Headers({
-                'Content-Type': 'application/json'
+        if (this.state.usernameValid && this.state.passwordValid) {
+            fetch("http://localhost:3000/user/signin", {
+                method: 'POST',
+                body: JSON.stringify({ user: this.state }),
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+            }).then(
+                (response) => response.json()
+            ).then((data) => {
+                this.props.setToken(data.sessionToken)
             })
-        }).then(
-            (response) => response.json()
-        ).then((data) => {
-            this.props.setToken(data.sessionToken)
-        })
+        }
         event.preventDefault()
     }
 
@@ -42,15 +77,16 @@ class Login extends Component {
         return (
             <div>
                 <h1>Login</h1>
-                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellat, atque nulla, soluta vero reprehenderit numquam incidunt, rem quaerat quos voluptatum perferendis. Distinctio culpa iste atque blanditiis placeat qui ipsa?</h6>
                 <Form onSubmit={this.handleSubmit}>
                     <FormGroup>
                         <Label for="username">Username</Label>
-                        <Input id="login_username" type="text" name="username" placeholder="enter username" onChange={this.handleChange} />
+                        <Input id="login_username" type="email" name="username" value={this.state.username} invalid={!this.state.usernameValid} placeholder="enter username" onChange={this.handleChange} />
+                        <FormFeedback>Enter a valid email</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">Password</Label>
-                        <Input id="login_password" type="password" name="password" placeholder="enter password" onChange={this.handleChange} />
+                        <Input id="login_password" type="text" name="password" value={this.state.password} invalid={!this.state.passwordValid} placeholder="enter password" onChange={this.handleChange} />
+                        <FormFeedback>Enter a password 6 characters or more</FormFeedback>
                     </FormGroup>
                     <Button type="submit"> Submit </Button>
                 </Form>
--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { 
-    Form, 
-    FormGroup, 
-    Label, 
-    Input, 
-    Button 
+import {
+    Form,
+    FormGroup,
+    FormFeedback,
+    Label,
+    Input,
+    Button
 } from 'reactstrap';
 
 class Signup extends Component {
@@ -12,44 +13,76 @@ class Signup extends Component {
         super(props)
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            usernameValid: true,
+            passwordValid: true
         };
     }
-    
+
     handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({ [name]: value },
+            () => {
+                this.validateField(name, value)
+            });
+
+    }
+
+    validateField(fieldName, value) {
+
+        let usernameValid = this.state.usernameValid;
+        let passwordValid = this.state.passwordValid;
+
+        switch (fieldName) {
+            case 'username':
+                usernameValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                break;
+            case 'password':
+                passwordValid = value.length >= 6;
+                break;
+            default:
+                break;
+        }
+
         this.setState({
-            [event.target.name]: event.target.value,
+            usernameValid: usernameValid,
+            passwordValid: passwordValid
         });
     }
-    
+
     handleSubmit = (event) => {
-        fetch("http://localhost:3000/user/createuser", {
-            method: 'POST',
-            body: JSON.stringify({user: this.state}),
-            headers: new Headers({
-                'Content-Type': 'application/json'
+        if (this.state.usernameValid && this.state.passwordValid) {
+            fetch("http://localhost:3000/user/createuser", {
+                method: 'POST',
+                body: JSON.stringify({ user: this.state }),
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+            }).then(
+                (response) => response.json()
+            ).then((data) => {
+                this.props.setToken(data.sessionToken)
             })
-        }).then(
-            (response) => response.json()
-        ).then((data) => {
-            this.props.setToken(data.sessionToken)
-        })
+        }
         event.preventDefault()
+
     }
-    
+
     render() {
         return (
             <div>
                 <h1>Sign Up</h1>
-                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellat, atque nulla, soluta vero reprehenderit numquam incidunt, rem quaerat quos voluptatum perferendis. Distinctio culpa iste atque blanditiis placeat qui ipsa?</h6>
                 <Form onSubmit={this.handleSubmit} >
                     <FormGroup>
                         <Label for="username">Username</Label>
-                        <Input id="username" type="text" name="username" placeholder="enter username" onChange={this.handleChange}/>
+                        <Input id="username" type="text" name="username" value={this.state.username} invalid={!this.state.usernameValid} placeholder="enter username" onChange={this.handleChange} />
+                        <FormFeedback>Enter a valid email</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">Password</Label>
-                        <Input id="signup_password" type="password" name="password" placeholder="enter password" onChange={this.handleChange}/>
+                        <Input id="signup_password" type="password" name="password" value={this.state.password} invalid={!this.state.passwordValid} placeholder="enter password" onChange={this.handleChange} />
+                        <FormFeedback>Enter a password 6 characters or more</FormFeedback>
                     </FormGroup>
                     <Button type="submit">Submit</Button>
                 </Form>
